3607a8f5bf87764807ef47f3ea496b815b46fdf3
mostafa kamal <mostafajunayed20@gmail.com>
2024-03-01 21:25:22 -0500

    Changed Styling of Experience

--- src/pages/index.js
+++ src/pages/index.js
@@ -141,19 +141,30 @@ export default function Home() {
           {ExperienceData.map((item, index) => {
             return (
               <div className="mx-auto">
-                <div className="flex flex-row justify-between">
-                  <p className="font-bold text-lg">{item.company_name}</p>
-                  <p className="font-bold text-lg">{item.start_date} - {item.end_date}</p>
-                </div>
-                <div className="flex flex-row justify-between">
-                  <p className="font-bold text-lg">{item.role}</p>
-                  <p className="font-bold text-lg">{item.location}</p>
-                </div>
-                <ul className="list-disc">
+                
+                <Card>
+                  <CardHeader className="flex flex-col justify-center">
+                    <div>
+                      <p className="font-bold text-lg">{item.company_name}{", "}{item.location}</p>
+                    </div>
+                    <div>
+                      <p>{item.role}</p>  
+                    </div>
+                    <div className="flex flex-row gap-3 items-center">
+                        <Chip className = "border-blue-700" color={"primary"} variant="dot" radius="sm">{item.start_date}</Chip>
+                        <Chip className = "border-grey-100" color={"primary"} variant="bordered" radius="sm">{"-"}</Chip>
+                        <Chip className = "border-blue-700" color={"primary"} variant="dot" radius="sm">{item.end_date}</Chip>  
+                      </div>
+                  </CardHeader>
+                  <CardBody>
+                    <ul className="list-disc">
                     {item.description.map(temp => (
                       <li>{temp}</li>
                     ))}
-                </ul>
+                    </ul>
+                  </CardBody>
+                </Card>
+                
                 <div className="flex flex-wrap gap-2 pt-3">
                   {item.technologies.map(item => (
                     <Chip>{item}</Chip>
