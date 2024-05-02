58ad0181885fb8ee98887f6ed2e895dbc228d3e4
mostafak2 <mostafajunayed20@gmail.com>
2024-05-01 23:07:08 -0400

    website update

--- src/lib/experienceData.js
+++ src/lib/experienceData.js
@@ -1,25 +1,35 @@
 const EXPERIENCE_DATA = [
-    {
-      company_name: "Stealth Startup",
-      role: "Part-Time Software Engineer",
-      location: "Remote",
-      start_date: "2024",
-      end_date: "Present",
-      description:"I am actively contributing to the development of a dynamic and innovative mobile and web application, seamlessly integrating it with Google APIs; the core technologies at the heart of this startup include Node.js for server-side development, React Native for cross-platform mobile application development, and DynamoDB for efficient and scalable NoSQL database management",
-      technologies: ["React", "Node", "React.Native", "DynamoDb"]
-    },
-  
-    {
-      company_name: "AVA Robotics",
-      role: "Intern",
-      location: "Cambridge",
-      start_date: "2018",
-      end_date: "2019",
-      description: "I worked with fellow engineers and developers where I conducted a series of tests on the robot. Additionaly, I contributed to both the assembly of the robot and the disassembly of its components for cleaning, testing, and eventual reuse.",
-      technologies: ["Python"]
-    }
+  {
+    company_name: "Stealth Startup",
+    role: "Software Engineer",
+    location: "Remote",
+    start_date: "2024",
+    end_date: "Present",
+    description:
+      "I am actively contributing to the development of a dynamic and innovative mobile and web application, seamlessly integrating it with Google APIs; the core technologies at the heart of this startup include Node.js for server-side development, React Native for cross-platform mobile application development, and DynamoDB for efficient and scalable NoSQL database management",
+    technologies: ["React", "Node", "React.Native", "DynamoDb"],
+  },
+  {
+    company_name: "Scale AI",
+    role: "AI Trainer",
+    location: "Remote",
+    start_date: "2024",
+    end_date: "Present",
+    description:
+      "I am contributing to making large language model provide the most accurate responses to user prompts. The majority of the work in this role involves prompt engineering, crafting SQL queries containing multiple joins and aggregations, tool invocation, fixing falty responses, and guiding the model to a correct response. I am also involved in reviewing work done by other attempters.",
+    technologies: ["SQL", "Prompt Engineering", "Python", "Java", "Javascript"],
+  },
+
+  {
+    company_name: "AVA Robotics",
+    role: "Intern",
+    location: "Cambridge",
+    start_date: "2018",
+    end_date: "2019",
+    description:
+      "I worked with fellow engineers and developers where I conducted a series of tests on the robot. Additionaly, I contributed to both the assembly of the robot and the disassembly of its components for cleaning, testing, and eventual reuse.",
+    technologies: ["Python"],
+  },
 ];
-
-
 
 export default EXPERIENCE_DATA;
